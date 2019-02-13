d01a5dc27e05a7d6f72b762942cc73f36459d6d0
sklinov <sergeyklinov@gmail.com>
2019-02-13 17:45:42 +0500

    gulp v1

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+ 
+gulp.task('default', () =>
+    gulp.src('src/**/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer())
+        .pipe(concat('all.css'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'))
+);
 
-exports.default = defaultTask
+exports.default = autoprefix;
